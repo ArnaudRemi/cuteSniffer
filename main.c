57231dc01a30d6460eeda693ca4d7327998a20d1
nepski <nepski.si@gmail.com>
2014-11-13 11:12:44 +0100

    proto raw socket + promisc mode

--- main.c
+++ main.c
@@ -5,12 +5,20 @@
 #include <netinet/udp.h>   //Provides declarations for udp header
 #include <netinet/tcp.h>   //Provides declarations for tcp header
 #include <netinet/ip.h>    //Provides declarations for ip header
+#include <netinet/in.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <arpa/inet.h>
 #include <net/ethernet.h>  //For ether_header
+#include <net/if.h>
+#include <netpacket/packet.h>
 
-int processPacket(char * buffer , int data_size){
+#include "cuteSniffer.h"
+
+int defineProtocol(char * buffer , int data_size){
     int j;
+    type_proto p = OTHER;
 
     printf("Ethernet Header\n");
 
@@ -24,20 +32,64 @@ int processPacket(char * buffer , int data_size){
     for (j=12;j<14;j++) 
         printf("%02x:",*(buffer+j));
     if (*(buffer+12)==8 && *(buffer+13)==0)
-        printf("IP\n");
+        p = IPV4;
+    if (*(buffer+12)==134 && *(buffer+13)==221)
+        p = IPV6;
     if (*(buffer+12)==8 && *(buffer+13)==6)
-        printf("ARP\n");
+        p = ARP;
+
+    //if IPVx -> is ICMP_Vx
+
+    printf("\n");
+    switch(p){
+        case IPV4:
+            printf("IPV4\n");
+            break;
+        case IPV6:
+            printf("IPV6\n");
+            break;
+        case ARP:
+            printf("ARP\n");
+            break;
+        default:
+            printf("UNKNOWN\n");
+    }
 
     printf("\nEnd of paquet\n");
-    return 0;
+    return p;
 }
 
-int main()
+int go_promiscuous(int sock, const char *device)
+{
+  struct ifreq ifr;
+  struct packet_mreq mr;
+  struct sockaddr_ll sll;
+  ifr.ifr_ifindex = 0;
+  strcpy(ifr.ifr_name, device);
+  if(ioctl(sock,SIOGIFINDEX,&ifr) < 0)
+  {
+    perror("ioctl error ");
+    return 1;
+  }
+
+  memset(&mr,0,sizeof(mr));
+  mr.mr_ifindex = ifr.ifr_ifindex;
+  mr.mr_type =  PACKET_MR_PROMISC;
+  if(setsockopt(sock, SOL_PACKET, PACKET_ADD_MEMBERSHIP, (char *)&mr, sizeof(mr)) < 0) {
+    perror("setsockopt error ");
+    return 1;
+  }
+  return 0;
+}
+
+int main(int ac, char **av)
 {
     int saddr_size , data_size;
     struct sockaddr saddr;
     struct in_addr in;
-    int sock_raw; 
+    int sock_raw;
+
+    
 
     unsigned char *buffer = (unsigned char *)malloc(65536); //Its Big!
      
@@ -46,12 +98,9 @@ int main()
     printf("Starting...\n");
     //Create a raw socket that shall sniff
     sock_raw = socket(PF_PACKET , SOCK_RAW , htons(ETH_P_ALL));
-    //sock_raw = socket(AF_INET , SOCK_RAW , IPPROTO_RAW);
-    //sock_raw = socket(AF_INET ,SOCK_RAW , IPPROTO_ICMP);
 
-    //sock_raw = socket(AF_NETLINK, SOCK_RAW, NETLINK_NETFILTER);
-    //sock_raw = socket(AF_NIT, SOCK_RAW, NITPROTO_RAW);
-    //fd = socket(PF_RAW, SOCK_RAW, RAWPROTO_SNOOP);
+    go_promiscuous(sock_raw, "wlan3");
+
     if(sock_raw < 0)
     {
         printf("Socket Error\n");
@@ -68,10 +117,10 @@ int main()
             printf("Recvfrom error , failed to get packets\n");
             return 1;
         }
-        else if(data_size > 0)
-            printf("OK!\n");
+/*        else if(data_size > 0)
+            printf("OK!\n");*/
         //Now process the packet
-        processPacket(buffer , data_size);
+        defineProtocol(buffer , data_size);
     }
 }
 
